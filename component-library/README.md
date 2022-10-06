a03e6daf65cbffd293b66fd411c8e66434e59e1b
jinseok9338 <jinseok9338@gmail.com>
2022-10-06 02:11:37 +0000

    creating yew app

--- component-library/README.md
+++
@@ -1,2 +0,0 @@
-This is my UI library for future reference
-Anyone can make a tweak about this and hope this suits your taste
---
+++ video-streaming/src/main.rs
@@ -0,0 +1,14 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html!(
+    <div>
+        {"Hi !!!"}
+    </div>
+    )
+}
+
+fn main() {
+    yew::start_app::<App>();
+}
